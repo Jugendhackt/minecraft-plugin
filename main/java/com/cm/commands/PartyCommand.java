2b18bd6c70d64582a2e10f917dcba2cf8a1f05bc
bot196 <61917941+bot196@users.noreply.github.com>
2020-11-07 16:34:55 +0100

    Update PartyCommand.java

--- main/java/com/cm/commands/PartyCommand.java
+++ main/java/com/cm/commands/PartyCommand.java
@@ -1,5 +1,6 @@
 package com.cm.commands;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -14,10 +15,12 @@ public class PartyCommand implements CommandExecutor {
 
         ArrayList<String> members = new ArrayList<String>();
 
-        if(args.length == 2) {
-            if(args[0].equalsIgnoreCase("invite")) {
+        if (args.length == 2) {
+            if (args[0].equalsIgnoreCase("invite")) {
                 members.add(args[1]);
+                player.sendMessage(ChatColor.GREEN + "Added " + args[1] + " to the partychat!");
             }
+
         }
 
         System.out.println(members);
